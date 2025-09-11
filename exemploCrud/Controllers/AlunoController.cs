beff87b65d3b38d53383890531e6ea6fa37b76f1
betomelo_9 <betomelo_9@hotmail.com>
2025-09-11 20:03:07 -0300

    codigo aula 1109 camada service

--- exemploCrud/Controllers/AlunoController.cs
+++ exemploCrud/Controllers/AlunoController.cs
@@ -1,5 +1,6 @@
 ﻿using exemploCrud.Models;
 using exemploCrud.Repository;
+using exemploCrud.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace exemploCrud.Controllers
@@ -8,22 +9,22 @@ namespace exemploCrud.Controllers
     [Route("api/[controller]")]
     public class AlunoController : ControllerBase
     {
-        private IAlunoRepository _alunoRepository;
+        private IAlunoService _alunoService;
 
-        public AlunoController(IAlunoRepository alunoRepository)
+        public AlunoController(IAlunoService alunoService)
         {
-            _alunoRepository = alunoRepository;
+            _alunoService = alunoService;
         }
 
         [HttpGet("alunos")]
         public ActionResult<List<Models.AlunoDTO>> ObterAlunos()
         {
-            return Ok(_alunoRepository.ObterTodos());
+            return Ok(_alunoService.ObterTodos());
         }
         [HttpGet("alunos/{cpf}")]
         public ActionResult<Models.AlunoDTO> ObterAlunoByCpf(string cpf)
         {
-            var resultado = _alunoRepository.ObterPorCpf(cpf);
+            var resultado = _alunoService.ObterPorCpf(cpf);
            
             if (resultado == null)
             {
@@ -34,41 +35,34 @@ namespace exemploCrud.Controllers
 
         [HttpPost("alunos")]
         public ActionResult<Models.AlunoDTO> CriarAluno([FromBody] Models.AlunoDTO novoAluno)
-        {
-
-            var resultado = _alunoRepository.ObterPorCpf(novoAluno.cpf);
-
-            if (resultado is not null)
-            {
-                return BadRequest("Aluno com este CPF já existe.");
-            }
-            _alunoRepository.Adicionar(novoAluno);
+        {          
+            _alunoService.Adicionar(novoAluno);
 
             return Ok("Aluno criado com sucesso");
         }
         [HttpPut("alunos/{cpf}")]
         public ActionResult<Models.AlunoDTO> UpdateAluno(string cpf, [FromBody] Models.AlunoDTO alunoAtualizado)
         {        
-            var alunoExistente = _alunoRepository.ObterPorCpf(cpf);
+            var alunoExistente = _alunoService.ObterPorCpf(cpf);
 
             if (alunoExistente == null)
             {
                 return NotFound("Aluno não encontrado");
             }
-            _alunoRepository.Atualizar(cpf, alunoAtualizado);
+            _alunoService.Atualizar(cpf, alunoAtualizado);
 
             return Ok("Aluno atualizado com sucesso");
         }
         [HttpDelete("alunos/{cpf}")]
         public ActionResult DeleteAluno(string cpf)
         {
-            var alunoExistente = _alunoRepository.ObterPorCpf(cpf);
+            var alunoExistente = _alunoService.ObterPorCpf(cpf);
 
             if (alunoExistente == null)
             {
                 return NotFound("Aluno não encontrado");
             }
-            _alunoRepository.Remover(cpf);
+            _alunoService.Remover(cpf);
 
             return NoContent();
         }
--- exemploCrud/Program.cs
+++ exemploCrud/Program.cs
@@ -1,4 +1,5 @@
 using exemploCrud.Repository;
+using exemploCrud.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IAlunoRepository, AlunoRepository>();
+builder.Services.AddSingleton<IAlunoService, AlunoService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
